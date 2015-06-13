71882415b03cb97c8ddd63ee4aaa45a654fea6cd
Sven Weisker <uucly@student.kit.edu>
2015-06-13 19:03:29 +0200

    test  to dropdown

--- src/main/java/com/germany/StartPage.java
+++ src/main/java/com/germany/StartPage.java
@@ -1,9 +1,17 @@
 package com.germany;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class StartPage extends WebPage{
@@ -12,8 +20,20 @@ public class StartPage extends WebPage{
 	
 	public StartPage(final PageParameters parameters){
 		super(parameters);
-		IModel<String> m = Model.of("");
-		m.setObject("Hallo");
-		add(new Label("test", m));
+		
+		Form form = new Form("form");
+		String test = "bla";
+		List<String> SEARCH_ENGINES = Arrays.asList(new String[] {
+				"Google", "Bing", "Baidu" });
+		 
+		//variable to hold the selected value from dropdown box,
+		//and also make "Google" is selected by default
+		String selected = "Google";
+		 
+		DropDownChoice<String> listSites = new DropDownChoice<String>(
+				"sites", new PropertyModel<String>(this, "selected"), SEARCH_ENGINES);
+		//form.add(listSites);
+		add(form);
+		
 	}
 }
