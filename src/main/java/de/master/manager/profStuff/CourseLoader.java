522593a98b589aefc5cc20784e1c74176a0399db
Sven Weisker <sweisker@web.de>
2015-11-14 11:51:43 +0100

    grade: little input stream refactoring.

--- src/main/java/de/master/manager/profStuff/CourseLoader.java
+++ src/main/java/de/master/manager/profStuff/CourseLoader.java
@@ -2,6 +2,9 @@ package de.master.manager.profStuff;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Function;
 import java.util.regex.Pattern;
@@ -13,41 +16,60 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CourseLoader implements ICourseLoader{
+public class CourseLoader implements ICourseLoader {
 
 	private static final long serialVersionUID = 1L;
 	private final List<ICourse> allCourses;
 
-	public CourseLoader(String pathToAllCourse){
-		
+	public CourseLoader(String pathToAllCourse) {
+
 		Function<String, ICourse> parseToModul = line -> {
 			String[] split = line.split(",");
 			return new ModulCourse(split[0], Double.parseDouble(split[1]));
 		};
-		
-		allCourses = Pattern.compile("\n").splitAsStream(loadFileInput(pathToAllCourse)).map(parseToModul).collect(Collectors.toList());
+
+		allCourses = Pattern.compile("\n").splitAsStream(loadFileInput(pathToAllCourse)).map(parseToModul)
+				.collect(Collectors.toList());
 	}
-	
+
 	/* methods */
-	
+
 	public List<ICourse> loadCourses(String fileName) {
 		String fileInput = loadFileInput(fileName);
-		return Pattern.compile(",").splitAsStream(fileInput).map(s -> allCourses.get(Integer.parseInt(s)-1)).collect(Collectors.toList());
+		return Pattern.compile(",").splitAsStream(fileInput).map(s -> allCourses.get(Integer.parseInt(s) - 1))
+				.collect(Collectors.toList());
 	}
-	
+
 	/**
 	 * 
-	 * @param fileName with file ending (e.g. fileName.txt)
+	 * @param fileName
+	 *            with file ending (e.g. fileName.txt)
 	 * @return input of file
 	 */
-	private String loadFileInput(String fileName){
+	private String loadFileInput(String fileName) {
 		FileSystemResourceLoader loader = new FileSystemResourceLoader();
 		Resource resource = loader.getResource("/src/main/resources/" + fileName);
-		try {
-			InputStream pflichtResource = resource.getInputStream();
-			return IOUtils.toString(pflichtResource, "UTF-8");
+		try (InputStream pflichtResource = resource.getInputStream();) {
+			return loadFileInputFromInputStream(pflichtResource);
 		} catch (IOException e) {
 			throw new RuntimeException("Cannot load file" + fileName, e);
+		}
+	}
+
+	private String loadFileInputFromInputStream(InputStream pflichtResource) {
+		try {
+			return IOUtils.toString(pflichtResource, "UTF-8");
+		} catch (IOException ioE) {
+			throw new RuntimeException("Cannot close inputstream", ioE);
+		} finally {
+			close(pflichtResource);
+		}
+	}
+
+	private static void close(InputStream stream) {
+		try {
+			stream.close();
+		} catch (IOException e) {
 		}
 	}
 
